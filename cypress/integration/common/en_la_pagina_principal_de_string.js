8b8f2d2b20b68f1a6bc48537e357e1e931f0a13d
Nelson Campos <nelsonramirocampos@gmail.com>
2022-02-04 16:12:58 -0300

    ...

--- cypress/integration/common/en_la_pagina_principal_de_string.js
+++ cypress/integration/common/en_la_pagina_principal_de_string.js
@@ -1,14 +1,6 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 const report = require('multiple-cucumber-html-reporter');
 
-
-var fs = require('fs');
-var params = {
-    "id":5,
-    "name":"Rey águila ceja blanca"
-}
-
 Given('en la pagina principal de {string}', (url) => {
     cy.visit(url).screenshot()
 })
-
