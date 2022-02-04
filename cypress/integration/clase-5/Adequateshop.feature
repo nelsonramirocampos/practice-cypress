22ce90eca82a171b7fd8f0fa63652bbe1f409b00
Nelson Campos <nelsonramirocampos@gmail.com>
2022-02-04 17:29:29 -0300

    finish clase 5

--- cypress/integration/clase-5/Adequateshop.feature
+++ cypress/integration/clase-5/Adequateshop.feature
@@ -4,3 +4,14 @@ Feature: Suite de TC para Adequateshop
 Scenario: Registrar usuario nuevo
     Given el nombre "Nelson" email "nelson@ggmail.com" y password "123456"
     When llamamos al servicio registration
+
+Scenario Outline: Realizar login de un usuario registrado
+    Given el usuario "<userName>" email "<userEmail>" y password "<userPassword>"
+    When llamamos al servicio login
+    Examples:
+        | userName | userEmail | userPassword |
+        | Nelson  | nelson@ggmail.com  | 123456  |
+
+Scenario: Obtener todos los usuarios
+    Given el token para el usuario "Nelson" email "nelson@ggmail.com" y password "123456"
+    When llamamos al servicio users
--- cypress/integration/clase-5/Adequateshop/adequateshop-steps.js
+++ cypress/integration/clase-5/Adequateshop/adequateshop-steps.js
@@ -2,6 +2,7 @@
 import { Given, When, Then} from "cypress-cucumber-preprocessor/steps";
 
 let bodyRegistro = require('./../../../support/contratos/contratoRegistro.json')
+let bodyLogin = require('./../../../support/contratos/contratoRegistro.json')
 
 Given('el nombre {string} email {string} y password {string}', (userName, userEmail, userPassword) => {
     bodyRegistro.name = userName
@@ -16,5 +17,67 @@ When('llamamos al servicio registration', () => {
         body: bodyRegistro,
     }).then((response) => {
         cy.log(JSON.stringify(response.body))
+    })
+})
+
+
+
+Given('el usuario {string} email {string} y password {string}', (userName, userEmail, userPassword) => {
+    bodyLogin.name = userName
+    bodyLogin.email = userEmail
+    bodyLogin.password = userPassword
+})
+
+When('llamamos al servicio login', () => {
+    cy.request({
+        method: "POST",
+        url: "http://restapi.adequateshop.com/api/authaccount/login",
+        body: bodyLogin,
+    }).then((response) => {
+        const resp_body = JSON.stringify(response.body)
+        cy.log(resp_body)
+
+        expect(response.status).to.be.eq(200)
+
+        Cypress.env('TOKEN', response.body.data.Token)
+        cy.log('Token: ' + Cypress.env('TOKEN'))
+    })
+})
+
+
+
+Given('el token para el usuario {string} email {string} y password {string}', (userName, userEmail, userPassword) => {
+    bodyLogin.name = userName
+    bodyLogin.email = userEmail
+    bodyLogin.password = userPassword
+
+    cy.request({
+        method: "POST",
+        url: "http://restapi.adequateshop.com/api/authaccount/login",
+        body: bodyLogin,
+    }).then((response) => {
+        const resp_body = JSON.stringify(response.body)
+        cy.log(resp_body)
+
+        expect(response.status).to.be.eq(200)
+
+        Cypress.env('TOKEN', response.body.data.Token)
+        //cy.log('Token: ' + Cypress.env('TOKEN'))
+    })
+})
+
+
+When('llamamos al servicio users', () => {
+    cy.request({
+        method: "GET",
+        url: "http://restapi.adequateshop.com/api/users?page=1",
+        headers:{
+            "Authorization": 'Bearer ' + Cypress.env('TOKEN')
+        }   
+    }).then((response) => {
+        const resp_body = JSON.stringify(response.body)
+        cy.log(resp_body)
+
+        expect(response.status).to.be.eq(200)
     })
 })
